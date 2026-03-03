0faa38f90959a5957afcd0e806908bca1771984a
Tyler Rhoads <tyrhoads@gmail.com>
2026-03-02 21:24:36 -0500

    finished

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,16 @@ use std::num::ParseIntError;
 /// Use the `parse` method to turn a string slice into an unsigned 32 bit integer.
 /// If parsing fails... return the resulting ParseIntError
 pub fn parse_an_int(raw_value: &str) -> Result<u32, ParseIntError> {
-    todo!()
         //
+        raw_value.parse::<u32>()
 }
 
 
 /// Attempt to divide by zero. If rhs is 0, return None, otherwise return some quotient.
 pub fn divide(lhs: i32, rhs: i32) -> Option<i32> {
-    todo!()
+   if rhs == 0 {
+        None
+    } else {
+         Some(lhs / rhs)
+    } 
 }
